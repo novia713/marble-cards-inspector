e86cb051d8accd09443c0e383f1a6ff5b71f533f
Christian Grieger <github@christiangrieger.de>
2020-11-26 23:54:58 +0100

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,15 +1,27 @@
 module.exports = {
     bot:{
-        set_status: true, // When enabled it sets a new activity status for the Discord bot
+        set_status: false, // When enabled it sets a new activity status for the Discord bot
         token: "DISCORD_BOT_TOKEN", // Discord bot token
-        anti_spam_time: 10, // Cooldown time in seconds between requests
+        anti_spam_time: 5, // Cooldown time in seconds between requests
         prefix: ".", // Bot command prefix
     },
     commands:{
-        c: ".c <url> # Check an URL for MarbleCards",
-        donate: ".donate # Get bot creator donation info"
+        c: ".check <url> # Check an URL for MarbleCards.",
+        donate: ".donate # Bot creator donation info."
     },
     messages: {
         status: "Commands: .help",
+        error: "please contact the bot developer. The command has triggered an error:",
+        not_valid: "this is not a valid command. Type .help for a list of commands.",
+        anti_spam: "please wait",
+        anti_spam_2: "seconds between your requests!",
+        bot_commands: "Bot commands",
+        no_bot_commands: "There is no command available.",
+        donate: "If you want to support the developer use the following Ethereum (ETC) address: `0xfDb4FDc4Eb9F7158B9Cae46f78357f3cBC8107C3`. Thank you!",
+        no_valid_url: "The given domain does not seem to be valid. Please check the list of allowed domains at `https://marble.cards/allowed-domains` and do not forget to put `https://` in front of the domain.",
+        not_marbled_url: "the url",
+        not_marbled_url_2: "can be marbled.",
+        marbled_url: "the url",
+        marbled_url_2: "is already marbled.",
     }
 };
