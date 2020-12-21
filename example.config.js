45074aab7dc799e7438bbab7037e301c8603745e
Christian Grieger <github@christiangrieger.de>
2020-12-21 22:49:01 +0100

    Update example config

--- example.config.js
+++ example.config.js
@@ -6,7 +6,10 @@ module.exports = {
         prefix: ".", // Bot command prefix
     },
     commands:{
-        c: ".check <url> # Check an URL for MarbleCards.",
+        check: ".check <url> # Check an URL for MarbleCards.",
+        wiki: ".wiki # MarbleCards Wiki.",
+        random: ".random # Post a random MarbleCard.",
+        card: ".card <number># Display MarbleCard by number.",
         donate: ".donate # Bot creator donation info."
     },
     anti_scam: ["uniswap is","uniswap is doing","uniswap airdrop"], // ALL LOWERCASE! -> If a record has one of the specified contents, the message is deleted by the bot.
@@ -18,11 +21,19 @@ module.exports = {
         anti_spam_2: "seconds between your requests!",
         bot_commands: "Bot commands",
         no_bot_commands: "There is no command available.",
-        donate: "If you want to support the developer use the following Ethereum (ETC) address: `0xfDb4FDc4Eb9F7158B9Cae46f78357f3cBC8107C3`. Thank you!",
-        no_valid_url: "The given domain does not seem to be valid. Please check the list of allowed domains at `https://marble.cards/allowed-domains` and do not forget to put `https://` in front of the domain.",
+        donate: "if you want to support the developer use the following Ethereum (ETH) address: `0xfDb4FDc4Eb9F7158B9Cae46f78357f3cBC8107C3`. Thank you!",
+        api_down: "the marble cards api is currently not available. please try again later.",
         not_marbled_url: "the url",
-        not_marbled_url_2: "can be marbled.",
+        not_marbled_url_2: "can be marbled. :green_circle:",
         marbled_url: "the url",
-        marbled_url_2: "is already marbled.",
+        marbled_url_2: "is already marbled. :red_circle:",
+        not_allowed: "the url",
+        not_allowed_2: "is not allowed or currently disabled. Please check the list of allowed domains at `https://marble.cards/allowed-domains` and do not forget to put `https://` in front of the domain. :yellow_circle:",
+        wiki: "if you have questions about Marble cards you are welcome to browse the wiki - https://marblecards.christiangrieger.de - . If you have any further questions, the community is very helpful. So don't be afraid to ask your questions.",
+        banned: "you got banned because of scam attempts.",
+        card_number_not_valid: "the requested card id is not valid.",
+        card_number_to_low: "the requested card number cannot be smaller than the number 1.",
+        card_number_to_high: "the requested card number cannot be bigger than the number",
+        card_number_to_high_2: "."
     }
 };
